09a708a7eb77cc80d4a29a8b99a9b23e21070445
Vi <liliput_j@yahoo.com>
2019-03-25 10:17:25 -0700

    Removed state from Login component

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,50 +1,25 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class Login extends Component {
-  state = {
-    email: "",
-    password: ""
-  };
-
-  onSubmit(e) {
+function Login() {
+  function onSubmit(e) {
     e.preventDefault();
 
-    const user = {
-      email: this.state.email,
-      password: this.state.password
-    };
+    // handle click
 
-    console.log(user);
+    // console.log(user);
   }
+  return (
+    <div className="login">
+      <h1>Log In</h1>
+      <form action="" onSubmit={onSubmit}>
+        <input type="email" placeholder="Email Address" />
+        <input type="password" placeholder="Password" />
+        <input type="submit" />
+      </form>
 
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-
-  render() {
-    return (
-      <div className="login">
-        <h1>Log In</h1>
-        <form action="" onSubmit={this.onSubmit}>
-          <input
-            type="email"
-            placeholder="Email Address"
-            value={this.state.email}
-            onChange={this.onChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.onChange}
-          />
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  }
+      {/* TODO:  Add Register info */}
+    </div>
+  );
 }
 
 export default Login;
