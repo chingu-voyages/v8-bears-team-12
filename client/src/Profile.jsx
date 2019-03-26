993941e164eb1407f0c14fa2c2dcd3c44e12ccb2
Jane Lee <sohyunlee53@gmail.com>
2019-03-25 23:22:56 -0700

    Refactored using React hooks

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,101 +1,82 @@
 import React, { useState } from 'react';
 
-// first write it as class then refactor using React hook
+function Profile() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [zipcode, setZipcode] = useState('');
+  const [interests, setInterests] = useState([]);
+  const [dietRestrictions, setDietRestrictions] = useState('');
 
-class Profile extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: '',
-      email: '',
-      password: '',
-      zipcode: '',
-      interests: [],
-      dietRestrictions: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-    console.log(this.state)
-  }
-
-  render() {
-    return (
-      <div>
-        <form>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br></br>
-          <label>
-            Email:
-            <input
-              type="text"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br></br>
-          <label>
-            Password:
-            <input
-              type="text"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br></br>
-          <label>
-            Zipcode:
-            <input
-              type="text"
-              name="zipcode"
-              value={this.state.zipcode}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br></br>
-          <label>
-            Interests:
-            <input
-              type="text"
-              name="interests"
-              value={this.state.interests}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br></br>
-          <label>
-            Diet Restrictions:
-            <input
-              type="text"
-              name="dietRestrictions"
-              value={this.state.dietRestrictions}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br></br>
-          <button type="button">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <label>
+          Password:
+          <input
+            type="text"
+            name="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <label>
+          Zipcode:
+          <input
+            type="text"
+            name="zipcode"
+            value={zipcode}
+            onChange={e => setZipcode(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <label>
+          Interests:
+          <input
+            type="text"
+            name="interests"
+            value={interests}
+            onChange={e => setInterests(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <label>
+          Diet Restrictions:
+          <input
+            type="text"
+            name="dietRestrictions"
+            value={dietRestrictions}
+            onChange={e => setDietRestrictions(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <button type="button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Profile;
