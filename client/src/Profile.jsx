0a19aacecfa1a08bc16d1533469fdeaa00e50d72
Jane Lee <sohyunlee53@gmail.com>
2019-03-25 23:08:31 -0700

    Profile form rendering and updating on change

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,7 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Profile() {
-  return <>Profile Component</>;
+// first write it as class then refactor using React hook
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      zipcode: '',
+      interests: [],
+      dietRestrictions: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Email:
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Password:
+            <input
+              type="text"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Zipcode:
+            <input
+              type="text"
+              name="zipcode"
+              value={this.state.zipcode}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Interests:
+            <input
+              type="text"
+              name="interests"
+              value={this.state.interests}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Diet Restrictions:
+            <input
+              type="text"
+              name="dietRestrictions"
+              value={this.state.dietRestrictions}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <button type="button">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Profile;
