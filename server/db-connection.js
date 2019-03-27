66d580f1da95a2daf195ccf8c08c5c5c03e68f5e
Jay T <jaytula@gmail.com>
2019-03-27 01:24:22 +0000

    Avoid mongoose deprecation warning for ensureIndex

--- server/db-connection.js
+++ server/db-connection.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
 
 const { NODE_ENV, ATLAS_DBURI } = process.env;
-const URI = NODE_ENV === 'test' ? global.__MONGO_URI__ : ATLAS_DBURI;
+const URI = NODE_ENV === "test" ? global.__MONGO_URI__ : ATLAS_DBURI;
 
 async function DbConnection() {
   const ret = await mongoose.connect(URI, { useNewUrlParser: true });
-
-  console.log('mongoose connected!');
 
   return ret;
 }
