7a516d70b34b7747f08ff7a5628431c383620e40
Jane Lee <sohyunlee53@gmail.com>
2019-03-28 14:28:37 -0700

    Made basic search form

--- client/src/RestaurantPicker.jsx
+++ client/src/RestaurantPicker.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
-function RestaurantPicker(props) {
 
+/*
+Will include a search box allow up to five restaurants to be added to a list. 
+It should also allow removal of restaurants from the list. 
+This is where the Yelp API will be used.
+*/
+
+function RestaurantPicker(props) {
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Search restaurants, ramen, hamburger, pizza..."
+          required
+        />
+        <input
+          type="text"
+          placeholder="Near zipcode or city"
+          required
+        />
+        <button
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default RestaurantPicker;
