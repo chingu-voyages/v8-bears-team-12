18d739ec3dbf8e7850f90f2d34a076fc2237dcb8
Jane Lee <sohyunlee53@gmail.com>
2019-03-28 14:36:36 -0700

    set states for searchTerm and location. Verified via console.log

--- client/src/RestaurantPicker.jsx
+++ client/src/RestaurantPicker.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 /*
 Will include a search box allow up to five restaurants to be added to a list. 
@@ -8,21 +7,33 @@ This is where the Yelp API will be used.
 */
 
 function RestaurantPicker(props) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [location, setLocation] = useState('');
+
+  function onSubmit(e) {
+    // should call the axios GET request here
+    e.preventDefault();
+  }
   return (
     <div>
       <form>
         <input
           type="text"
+          value={searchTerm}
           placeholder="Search restaurants, ramen, hamburger, pizza..."
+          onChange={e => setSearchTerm(e.target.value)}
           required
         />
         <input
           type="text"
+          value={location}
           placeholder="Near zipcode or city"
+          onChange={e => setLocation(e.target.value)}
           required
         />
         <button
           type="submit"
+          onSubmit={onSubmit}
         >
           Search
         </button>
