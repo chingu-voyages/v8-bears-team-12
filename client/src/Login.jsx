e953ae8a259e55723e402074b97ed2f6730319a2
Vi <liliput_j@yahoo.com>
2019-03-28 11:09:04 -0700

    Refactored Login component using react hooks

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   function onSubmit(e) {
     e.preventDefault();
 
@@ -12,8 +15,16 @@ function Login() {
     <div className="login">
       <h1>Log In</h1>
       <form action="" onSubmit={onSubmit}>
-        <input type="email" placeholder="Email Address" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="email"
+          placeholder="Email Address"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={e => setPassword(e.target.value)}
+        />
         <input type="submit" />
       </form>
 
