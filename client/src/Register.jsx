efe9c73ce109ea5c56e6819115c41734b0e4c1de
Vi <liliput_j@yahoo.com>
2019-03-28 21:02:07 -0700

    Added more form fields and functionality for diet options

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -6,29 +6,69 @@ function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [password2, setPassword2] = useState('');
+  const [zipcode, setZipcode] = useState('');
+  const [interests, setInterests] = useState([]);
+  const [dietRestrictions, setDietRestrictions] = useState('');
+  const [optionOther, setOptionOther] = useState(false);
+
+  const otherOption = React.createRef();
+
+  const options = [
+    'None a.k.a. I eat anything and everything',
+    'Vegan',
+    'Vegetarian',
+    'GlutenFree',
+  ];
+
+  function handleOptions(e) {
+    if (e.target.value === 'Other...') {
+      setOptionOther(true);
+    } else {
+      setDietRestrictions(e.target.value);
+      setOptionOther(false);
+    }
+  }
 
   function onSubmit(e) {
     e.preventDefault();
 
-    const newUser = {
-      firstName,
-      lastName,
-      email,
-      password,
-    };
+    if (
+      password === password2 &&
+      (dietRestrictions !== '' && dietRestrictions !== 'Choose one...')
+    ) {
+      // create new user
+      const newUser = {
+        firstName,
+        lastName,
+        email,
+        password,
+        zipcode,
+        interests,
+        dietRestrictions,
+      };
 
-    if (password !== password2) {
-      alert("Passwords don't match!");
-      setPassword('');
-      setPassword2('');
-    } else {
       console.log(newUser);
 
+      // reset all fields
       setFirstName('');
       setLastName('');
       setEmail('');
       setPassword('');
       setPassword2('');
+      setZipcode('');
+      setInterests([]);
+      setDietRestrictions('');
+      setOptionOther(false);
+      otherOption.current.value = '';
+    }
+    if (password !== password2) {
+      alert("Passwords don't match!");
+      setPassword('');
+      setPassword2('');
+    }
+
+    if (dietRestrictions === '' || dietRestrictions === 'Choose one...') {
+      alert('Please choose an option for diet...');
     }
   }
 
@@ -40,6 +80,7 @@ function Register() {
       <form onSubmit={onSubmit}>
         <div />
         <div>
+          <label>First Name: </label>
           <input
             type="text"
             placeholder="First Name"
@@ -49,6 +90,7 @@ function Register() {
           />
         </div>
         <div>
+          <label>Last Name: </label>
           <input
             type="text"
             placeholder="Last Name"
@@ -58,6 +100,7 @@ function Register() {
           />
         </div>
         <div>
+          <label>Email: </label>
           <input
             type="email"
             placeholder="Email Address"
@@ -67,6 +110,7 @@ function Register() {
           />
         </div>
         <div>
+          <label>Password1: </label>
           <input
             type="password"
             placeholder="Password"
@@ -76,12 +120,62 @@ function Register() {
           />
         </div>
         <div>
+          <label>Password2: </label>
           <input
             type="password"
             placeholder="Confirm Password"
             value={password2}
             onChange={e => setPassword2(e.target.value)}
             required
+          />
+        </div>
+        <div>
+          <label>Zipcode: </label>
+          <input
+            type="text"
+            placeholder="Your zipcode"
+            value={zipcode}
+            onChange={e => setZipcode(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Interests: </label>
+          <input
+            type="text"
+            placeholder="Enter interests"
+            value={interests}
+            onChange={e => setInterests(e.target.value)}
+            required
+          />
+          <br />
+          <small>upto 5 separated by commas</small>
+        </div>
+        <div>
+          <label>Diet Restrictions: </label>
+          <select
+            value={dietRestrictions}
+            // onChange={e => setDietRestrictions(e.target.value)}
+            onChange={e => handleOptions(e)}
+          >
+            <option>Choose one...</option>
+            {options.map((i, j) => {
+              return (
+                <option key={i} value={i}>
+                  {i}
+                </option>
+              );
+            })}
+            <option>Other...</option>
+          </select>
+          <br />
+          <input
+            type="text"
+            style={{
+              display: optionOther ? 'block' : 'none',
+            }}
+            ref={otherOption}
+            onChange={e => setDietRestrictions(e.target.value)}
           />
         </div>
         <input type="submit" />
