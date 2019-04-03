7ac4e71ed32efa57c8e143b4a1252e91789aaca5
Jay T <jaytula@gmail.com>
2019-04-02 20:50:08 -0700

    Implement CLI pm-users-rm
    To delete user by username

--- pm/pm-users.js
+++ pm/pm-users.js
@@ -3,6 +3,7 @@ let program = require("commander");
 program
   .command("list", "user listing")
   .command("add <username> <email> <password>", "add a user")
+  .command("rm <username>", "remove a user")
   .parse(process.argv);
 
 !program.commands.map(cmd => cmd._name).includes(program.args[0]) &&
