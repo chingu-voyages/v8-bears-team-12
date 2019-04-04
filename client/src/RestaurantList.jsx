f0e9dc36d9eed2b0b1d4b830f3d6e6d716428b7c
Jane Lee <sohyunlee53@gmail.com>
2019-04-04 14:29:14 -0700

    New component for rendering restaurants list from Yelp API call

---
+++ client/src/RestaurantList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Restaurant from './Restaurant';
+
+function RestaurantList({ restaurantList }) {
+  return (
+    <div>
+      <ul>
+        {restaurantList.map((restaurant, i) => (
+          <Restaurant restaurant={restaurant} key={i} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+RestaurantList.propTypes = {
+  restaurantList: PropTypes.arrayOf(PropTypes.object),
+};
+
+RestaurantList.defaultProps = {
+  restaurantList: [],
+};
+
+export default RestaurantList;
