edd7ad8804edcd2e9381068b3f12817b68f6a589
Jane Lee <sohyunlee53@gmail.com>
2019-04-04 14:28:47 -0700

    New component for each restaurant listing

---
+++ client/src/Restaurant.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios';
+// import PropTypes from 'prop-types';
+
+function Restaurant(props) {
+  const { name, image_url, url, rating, location, phone} = props;
+
+  async function handleClick() {
+    const response = await axios.post('/api/restaurant-add', { restaurant });
+  }
+
+  return (
+    <div>
+      {name}
+      {/* {rating}
+      {location}
+      {phone} */}
+      <button type="button" onClick={handleClick}> Add </button>
+    </div>
+  );
+}
+
+// Restaurant.propTypes = {
+//   restaurant: PropTypes.object,
+// }
+
+// Restaurant.defaultProps = {
+//   restaurant: {},
+// };
+
+export default Restaurant;
