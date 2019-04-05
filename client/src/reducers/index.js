83677cfd89bceba06c4aa8b6f0f5296a2a3b5580
Jay T <jaytula@gmail.com>
2019-04-04 23:44:34 -0700

    Remove trailing spaces

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,11 +5,11 @@ const { LOGIN, LOGOUT } = actionTypes;
 const reducer = (state = { loggedIn: false, loading: true }, action) => {
   switch (action.type) {
     case LOGIN:
-      return { 
-        loggedIn: true, 
-        loading: false, 
-        username: action.username, 
-        restaurantList: action.restaurantList
+      return {
+        loggedIn: true,
+        loading: false,
+        username: action.username,
+        restaurantList: action.restaurantList,
       };
     case LOGOUT:
       return { loggedIn: false, loading: false, username: null };
