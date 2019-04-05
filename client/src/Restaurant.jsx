02bea0f8a13b970c001ecd490e6add025cba23de
Jay T <jaytula@gmail.com>
2019-04-04 22:46:27 -0700

    Fix restaurant destructuring

--- client/src/Restaurant.jsx
+++ client/src/Restaurant.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import axios from 'axios';
 // import PropTypes from 'prop-types';
 
-function Restaurant(props) {
-  const { name, image_url, url, rating, location, phone} = props.restaurant;
+function Restaurant({ restaurant }) {
+  const { name, image_url, url, rating, location, phone} = restaurant;
 
   async function handleClick() {
-    const response = await axios.post('/api/restaurant-add', { restaurant });
+    await axios.post('/api/restaurant-add', { restaurant });
   }
 
   return (
