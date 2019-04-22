7014bfba471c7f9fb68bb757c6aee87b44e41598
Jay T <jaytula@gmail.com>
2019-04-22 09:11:40 -0700

    Turn off a couple of eslint rules

--- client/src/.eslintrc.js
+++ client/src/.eslintrc.js
@@ -15,9 +15,9 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
+    'object-curly-newline': 'off',
+    'no-underscore-dangle': 'off',
   },
 };
