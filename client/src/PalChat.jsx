2828f235ff7948d5d783cb1b7c7b9a7282dccba0
Jay T <jaytula@gmail.com>
2019-04-24 20:25:48 -0700

    Add propTypes to PalChat

--- client/src/PalChat.jsx
+++ client/src/PalChat.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
 
 function PalChat({ match, pals }) {
   const { palId } = match.params;
   const pal = pals.find(e => e._id === palId);
 
   return (
-    <div>
+    <div className="pal-chat">
       <h1>Pal Chat: {pal.name}</h1>
+      <div className="message-window" />
+      <form>
+        <TextField label="Your Message" variant="outlined" />
+      </form>
     </div>
   );
 }
 
 PalChat.propTypes = {
   pals: PropTypes.arrayOf(PropTypes.object),
+  match: PropTypes.objectOf({
+    params: PropTypes.objectOf({
+      palId: PropTypes.string,
+    }),
+  }),
 };
 
 PalChat.defaultProps = {
   pals: [],
+  match: { params: {} },
 };
 
 const mapStateToProps = ({ profile }) => ({
