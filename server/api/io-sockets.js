71f23d6a1c087790d64447b8cd349ee9a76497b4
Jay T <jaytula@gmail.com>
2019-04-25 20:31:58 -0700

    Change io-sockets sockets to an array

--- server/api/io-sockets.js
+++ server/api/io-sockets.js
@@ -1,17 +1,16 @@
-let sockets = {};
+let sockets = [];
 
 module.exports = {
   getSockets: userIds => {
-    return userIds.flatMap(userId => sockets[userId]);
+    return sockets.filter(socket =>
+      userIds.some(e => e.equals(socket.user._id)),
+    );
   },
   addSocket: socket => {
-    const id = socket.user._id.toString();
-    if (!sockets[id]) sockets[id] = [];
-    sockets[id].push(socket);
+    sockets.push(socket);
   },
   removeSocket: socket => {
-    const id = socket.user._id.toString();
-    sockets[id] = sockets[id].filter(s => s.id !== socket.id);
+    sockets = sockets.filter(e => !socket.user._id.equals(e.user._id));
   },
   getAllSockets: () => {
     return sockets;
