7c41a337608e9b9d97790addf04cc77319e4c570
Jane Lee <sohyunlee53@gmail.com>
2019-04-27 14:32:12 -0700

    rendering background image & welcome guide texts

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -4,10 +4,46 @@ import PropTypes from 'prop-types';
 
 import Login from './Login';
 import Dashboard from './Dashboard';
+import backgroundImage from './media/meal.jpg';
+
+const styles = {
+  background : {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    height: '100vh',
+    width: '100vw',
+    opacity: 0.9,
+  }
+}
 
 function Home({ loggedIn, loading }) {
-  if (loading) return <div>loading</div>;
-  return <>{loggedIn ? <Dashboard /> : <Login />}</>;
+  if (loading) {
+    return <div>loading</div>;
+  }
+
+  return (
+    <div style={styles.background}>
+      <div>
+        <h1> Welcome to Pal-a-table! </h1>
+        <li>
+          Make your profile
+        </li>
+        <li>
+          Browse restaurants you want to try out
+        </li>
+        <li>
+          Search by your location to find nearby dining pals
+        </li>
+        <li>
+          Add pals and start chatting
+        </li>
+        <li>
+          Eat together and have fun!
+        </li>
+      </div>
+      {loggedIn ? <Dashboard /> : <Login />}
+    </div>
+  );
 }
 
 const mapStateToProps = ({ profile }) => ({
