021aec9d04949be2abf25bcfb763276f2bcfe182
Jay T <jaytula@gmail.com>
2019-04-28 02:01:55 -0700

    Filter messages that don't populate sender

--- server/api/socket-emitters.js
+++ server/api/socket-emitters.js
@@ -2,11 +2,18 @@ const Message = require('../models/Message');
 
 async function emitNewMessages(socket) {
   const { user } = socket;
-  const newMessages = await Message.find({
-    users: user._id,
-    sender: { $ne: user._id },
-    'message.read': false,
+  let newMessages = await Message.find(
+    {
+      users: user._id,
+      sender: { $ne: user._id },
+      'message.read': false,
+    },
+    { users: false },
+  ).populate({
+    path: 'sender',
+    select: 'name _id',
   });
+  newMessages = newMessages.filter(e => e.sender);
   socket.emit('SET_NEW_MESSAGES', newMessages);
 }
 
