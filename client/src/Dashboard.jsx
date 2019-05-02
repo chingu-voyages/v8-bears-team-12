97af7860057e832c7d3d700ceaea5cc343a59683
Jay T <jaytula@gmail.com>
2019-05-02 14:23:34 -0700

    Added PageHeader to Dashboard

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -4,30 +4,44 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
 import DiningMateList from './DiningMateList';
 
 import { setDiningMates } from './actionCreators';
+import PageHeader from './PageHeader';
+
+const styles = {
+  root: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+};
 
 function Dashboard({
+  classes,
   searchCity,
   searchState,
   dispatchSetDiningMates,
-  diningMates,
+  diningMates
 }) {
   useEffect(() => {
     if (searchCity) dispatchSetDiningMates();
   }, []);
 
   return (
-    <div className="dashboard-tab">
+    <div className={classes.root}>
+      <PageHeader>Dashboard</PageHeader>
       <div className="search-area-info">
         <h4 style={{ color: 'rgb(19, 73, 134)' }}>
           Search Area:
           <span
             style={{
               color: 'black',
-              borderBottom: '2px solid rgb(19, 73, 134) ',
+              borderBottom: '2px solid rgb(19, 73, 134) '
             }}
           >
             {searchCity}, {searchState}
@@ -43,22 +57,24 @@ function Dashboard({
 }
 
 Dashboard.propTypes = {
+  classes: PropTypes.shape({}),
   searchCity: PropTypes.string,
   searchState: PropTypes.string,
   searchLocation: PropTypes.shape({
     type: PropTypes.string,
-    coordinates: PropTypes.arrayOf(PropTypes.number),
+    coordinates: PropTypes.arrayOf(PropTypes.number)
   }),
   dispatchSetDiningMates: PropTypes.func,
-  diningMates: PropTypes.arrayOf(PropTypes.object),
+  diningMates: PropTypes.arrayOf(PropTypes.object)
 };
 
 Dashboard.defaultProps = {
+  classes: {},
   searchCity: '',
   searchState: '',
   searchLocation: { type: '', coordinates: [] },
   dispatchSetDiningMates: () => {},
-  diningMates: [],
+  diningMates: []
 };
 
 const mapStateToProps = state => ({
@@ -66,14 +82,14 @@ const mapStateToProps = state => ({
   searchCity: state.profile.searchCity,
   searchState: state.profile.searchState,
   searchLocation: state.profile.searchLocation,
-  diningMates: state.dashboard.diningMates,
+  diningMates: state.dashboard.diningMates
 });
 
 const mapDispatchToProps = {
-  dispatchSetDiningMates: setDiningMates,
+  dispatchSetDiningMates: setDiningMates
 };
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-)(Dashboard);
+  mapDispatchToProps
+)(withStyles(styles)(Dashboard));
