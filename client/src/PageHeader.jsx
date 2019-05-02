a200461b528c27925be42039d76260cd95612b39
Jay T <jaytula@gmail.com>
2019-05-02 12:07:09 -0700

    Create PageHeader component

---
+++ client/src/PageHeader.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    padding: '8px'
+  }
+};
+function PageHeader({ children, classes }) {
+  return (
+    <Typography className={classes.root} variant="h4">
+      {children}
+    </Typography>
+  );
+}
+
+PageHeader.propTypes = {
+  classes: PropTypes.shape({}),
+  children: PropTypes.string
+};
+
+PageHeader.defaultProps = {
+  classes: {},
+  children: {}
+};
+export default withStyles(styles)(PageHeader);
