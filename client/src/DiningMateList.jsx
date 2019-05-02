7ecbfa1d0f94631a05851921699f7d3bcf3a1883
Jay T <jaytula@gmail.com>
2019-05-01 22:27:33 -0700

    Use css grid to layout DiningMateList

--- client/src/DiningMateList.jsx
+++ client/src/DiningMateList.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
 import DiningMate from './DiningMate';
 
-function DiningMateList(props) {
-  const { diningMates } = props;
+const styles = theme => ({
+  root: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    padding: '8px'
+  },
+  list: {
+    display: 'grid',
+    width: '100%',
+    gridTemplateColumns: '100%',
+    [theme.breakpoints.up('lg')]: {
+      gridTemplateColumns: '50% 50%'
+    }
+  }
+});
 
+function DiningMateList({ diningMates, classes }) {
   const diningMateElems = diningMates.map(e => (
     <DiningMate key={e._id} pal={e} />
   ));
-  return <div className="dining-mate-list">{diningMateElems}</div>;
+  return <div className={classes.list}>{diningMateElems}</div>;
 }
 
 DiningMateList.propTypes = {
@@ -20,4 +40,4 @@ DiningMateList.defaultProps = {
   diningMates: []
 };
 
-export default DiningMateList;
+export default withStyles(styles)(DiningMateList);
