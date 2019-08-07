43edb9c626bec4a88d1363766744d3d2477dfc19
Jay Tulathimutte <jaytula@gmail.com>
2019-08-07 09:35:31 -0700

    Destructure to get CleanWebpackPlugin from require because of breaking change from 2.x to 3.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const mode = process.env.NODE_ENV;
 const DEBUG = mode !== 'production';
