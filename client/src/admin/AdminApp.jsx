a154f74086507aef1aad152328c57b54b47a3fd8
Jay T <jaytula@gmail.com>
2019-08-10 23:44:19 -0700

    Have UserList display just 5 fields

--- client/src/admin/AdminApp.jsx
+++ client/src/admin/AdminApp.jsx
@@ -4,11 +4,13 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
+import {UserList} from './resources/users';
+
 const dataProvider = jsonServerProvider('/api/admin')
 
 function AdminApp() {
     return (<Admin dataProvider={dataProvider}>
-       <Resource name="users" list={ListGuesser} />
+       <Resource name="users" list={UserList} />
     </Admin>);
 }
 
