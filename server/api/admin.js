0f8c0fa373a7f2cdf296fa1556a3f4c431ee95e2
Jay T <jaytula@gmail.com>
2019-08-10 23:43:10 -0700

    Have users GET_MANY return just id, email, name, firstName, lastName

--- server/api/admin.js
+++ server/api/admin.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 module.exports = app => {
     const usersRouter = express.Router();
     usersRouter.get('', async (req, res) => {
-        const users = await User.find({}).lean();
+        const users = await User.find({}, {_id: 1, email: 1, name: 1, firstName: 1, lastName: 1}).lean();
         users.forEach(u => {u.id = u._id; delete u._id; });
 
         res.header('X-Total-Count', users.length);
