500ce18bc091aad0d213c8bbc58dcff450e80f78
Jay T <jaytula@gmail.com>
2019-08-10 20:44:16 -0700

    Add component admin/AdminApp

---
+++ client/src/admin/AdminApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AdminApp() {
+    return <div>AdminApp</div>
+}
+
+export default AdminApp;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AdminApp from './admin/AdminApp';
 
 function renderApp() {
   const root = document.getElementById('root');
   if (root) ReactDOM.render(<App />, root);
+  const admin = document.getElementById('admin');
+  if (admin) ReactDOM.render(<AdminApp></AdminApp>, admin);
+
 }
 
 renderApp();
